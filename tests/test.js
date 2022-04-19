bbce50957f2f5a367b4d0dbe28e55c0993e2cd03
OG-Gardavsky <ogardavsky@gmail.com>
2022-04-19 23:12:44 +0200

    made function + file for data

--- tests/test.js
+++ tests/test.js
@@ -16,7 +16,6 @@ const baseUrl = 'https://rickandmortyapi.com/api'
 
 
 const succesCode = 200
-
 const requiredKeys =
     ['id', 'name', 'status', 'species', 'type', 'gender', 'origin', 'location', 'image', 'episode', 'url', 'created']
 
@@ -24,39 +23,49 @@ const requiredKeys =
 
 
 
-describe('first test', () => {
+const firstTest = (characterId) =>   {
 
-    describe('basic testik', () => {
-        it('Neco random', async () => {
+    if (characterId > 10 && characterId < 1) {
+        throw new Error('Choose number in range 1 - 10')
+    }
 
-            const res = await axios.get(makeEndpoint('/character/1'))
-            
-            
-            expect(res.status).to.equal(succesCode)
-            const resKeys = Object.keys(res.data)
+    describe('Positive tests', () => {
 
-            const extraKeys =[]
-            resKeys.forEach( key => {
-                if(!requiredKeys.includes(key)) {
-                    extraKeys.push(key)
-                }
+        describe('Testing of status code + object keys', () => {
+            it('Neco random', async () => {
+
+                console.log(`\n    we are testing character with id: ${characterId} `)
+
+                const res = await axios.get(makeEndpoint(`/character/${characterId}`))
+
+                expect(res.status).to.equal(succesCode)
+                const resKeys = Object.keys(res.data)
+
+                const extraKeys =[]
+                resKeys.forEach( key => {
+                    if(!requiredKeys.includes(key)) {
+                        extraKeys.push(key)
+                    }
+                })
+                assert(extraKeys.length === 0, `the following keys - " ${extraKeys} " should not be in response.`)
+
+                missingKeys = []
+                requiredKeys.forEach( key => {
+                    if(!resKeys.includes(key)) {
+                        missingKeys.push(key)
+                    }
+                })
+                assert(missingKeys.length === 0, `the following keys - " ${missingKeys} " are missing in response.`)
+
+                console.log(`    whoose name is: ${res.data.name} `)
             })
-            assert(extraKeys.length === 0, `the following keys - " ${extraKeys} " should not be in response.`)
-
-            missingKeys = []
-            requiredKeys.forEach( key => {
-                if(!resKeys.includes(key)) {
-                    missingKeys.push(key)
-                }
-            })
-
-            console.log(missingKeys)
-
-            assert(missingKeys.length === 0, `the following keys - " ${missingKeys} " are missing in response.`)
-
-
-
         })
+
+
+
     })
 
-})
+}
+
+
+firstTest(1)
